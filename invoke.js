9ac4299b0b26df51a8448f3d04e882275a88411e
Sean Tracey <seanmtracey@gmail.com>
2017-12-12 18:43:04 +0000

    Add a test file for invocing the function locally.

---
+++ invoke.js
@@ -0,0 +1,15 @@
+require('dotenv').config({silent : false});
+const fs = require('fs');
+
+const I = require(`${__dirname}/index.js`);
+
+I({
+    file : process.env.TEST_FILE,
+    STORAGE_KEY :  process.env.STORAGE_KEY,
+    OBJECT_STORAGE_ENDPOINT : process.env.OBJECT_STORAGE_ENDPOINT,
+    OBJECT_INSTANCE_ID : process.env.OBJECT_INSTANCE_ID,
+    processName : 'Barcelona',
+    divisionLevel : "0",
+    bucketName : process.env.BUCKETNAME,
+    functionURL : process.env.INVOCATION_FUNCTION_URL
+});
